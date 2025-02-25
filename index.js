0ea3edfdd14c913604ae336d9685c7cbd93541d9
Mikheil-Abuladze <abuladzemikheil1995@gmail.com>
2025-02-25 13:05:23 +0400

    finished 1st question

---
+++ index.js
@@ -0,0 +1,11 @@
+function testNum(a) {
+  if (a % 10 === 0) {
+    result = "true";
+  } else {
+    result = "false";
+  }
+  return result;
+}
+console.log(testNum(10));
+
+// ==========================
